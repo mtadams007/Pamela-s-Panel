07a6a8a6ebb2c44b8fecc93742e1c43b608cbf17
Michael Adams <mtadams007@gmail.com>
2018-05-15 10:41:40 -0400

    commented out all of js

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,11 +1,11 @@
-(function($) {
-$(".nav-link").hover(function(){
-   $(this).addClass("coloring");
-}, function(){
-  $(this).removeClass("coloring");
-});
-})(jQuery);
-
-{
-  let form;
-}
+// (function($) {
+// $(".nav-link").hover(function(){
+//    $(this).addClass("coloring");
+// }, function(){
+//   $(this).removeClass("coloring");
+// });
+// })(jQuery);
+//
+// {
+//   let form;
+// }
